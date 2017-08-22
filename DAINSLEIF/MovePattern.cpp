4b259e911d6b13e93712ec0916538dde41369f4c
Xenon <xenon2787@gmail.com>
2017-08-23 08:15:45 +0900

    弾の挙動に少しの「ブレ」をいれました

--- DAINSLEIF/MovePattern.cpp
+++ DAINSLEIF/MovePattern.cpp
@@ -1,4 +1,5 @@
 #include "MovePattern.h"
+#include "Siv3D.hpp"
 
 
 Vec2 MovePattern::straight_up(const Bullet & bullet)
@@ -9,10 +10,11 @@ Vec2 MovePattern::straight_up(const Bullet & bullet)
 
 std::function<Vec2(const Bullet&)> MovePattern::straight(const Player & player)
 {
-	return [direction=Vec2(), &player](const Bullet & c) mutable {
+	return[direction = Vec2(), &player](const Bullet & c) mutable {
 		if (direction.isZero()) {
-			direction = (Mouse::Pos() - player.get_position()).normalize();
+			direction = (Mouse::Pos() - player.get_position()).normalize() + Vec2{ Random(-0.1,0.1), Random(-0.1,0.1) };
 		}
-		return direction*8;
+
+		return direction *8;
 	};
 }
