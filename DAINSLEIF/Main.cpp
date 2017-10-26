2789ede05d5b3dc122a49e60ac0180dcd878c6c5
Xenon <xenon2787@gmail.com>
2017-10-26 21:22:10 +0900

    タイトル画面の追加
    ＞それに伴い各シーンで共用する画面の基礎フレームをswitch文の外に出してあります
    ＞選択肢はマウスオーバー時に点滅しますがクリック時のイベントはまだ設定してないです
    ＞"Game Start"と"Credits"の間が空いているのは後々"Game Start"を"New Game"と"Load Game"の2つに分ける予定

--- DAINSLEIF/Main.cpp
+++ DAINSLEIF/Main.cpp
@@ -9,7 +9,7 @@ void Main()
 	Window::SetTitle(L"DAINSLEIF");
 	Window::Resize(800, 600);
 	Window::SetStyle(WindowStyle::NonFrame);
-	Scene gamemode = Scene::Stage;
+	Scene gamemode = Scene::Title;
 
 
 	// フォントファイルを一時的にインストール
@@ -48,6 +48,19 @@ void Main()
 	const Font whiteBase10(10, L"GauFontWhiteBase");
 
 
+	const Texture title(L"assets/img/title.png");
+	const Rect gameStart = overDrive20(L"Game Start").region(80, 320);
+	const Rect credits = overDrive20(L"Credits").region(80, 440);
+	const Rect exit = overDrive20(L"Exit").region(80, 500);
+
+	int textAlpha_0 = 255;
+	int textAlpha_2 = 255;
+	int textAlpha_3 = 255;
+	bool isTextAlpha_0 = true;
+	bool isTextAlpha_2 = true;
+	bool isTextAlpha_3 = true;
+
+
 	//メニュー関連
 	const Rect rect(0, 0, 800, 600);
 	const Rect menubar(0, 0, 800, 30);
@@ -65,52 +78,135 @@ void Main()
 
 	while (System::Update())
 	{
+		void updateWindow(); {
+			//closeをクリックで閉じる
+			if (close.leftClicked)
+			{
+				System::Exit();
+			}
+			//ウインドウの移動
+			if (menubar.leftClicked)
+			{
+				// メニューをクリックしたらつかむ
+				if (menubarGrabbed == false)
+				{
+					//メニューつかんでないときマウス座標更新
+					mousePos = Mouse::Pos();
+
+				}
+				menubarGrabbed = true;
+
+			}
+			else if (Input::MouseL.released)
+			{
+				// マウスのクリックが離されたら離す
+				menubarGrabbed = false;
+			}
+			else if (menubarGrabbed)
+			{
+				// つかんでいる間はマウスのポジションに
+				Window::SetPos(Mouse::ScreenPos() - mousePos);
+			}
+
+		}
+		void drawWindow(); {
+			rect.draw(Color(255, 255, 255));
+		}
 		switch (gamemode)
 		{
 		case Scene::Title: {
 
-			//２つのフォントでタイトル描き比べしてるだけ
-			overDrive(title).draw();
-			whiteBase(title).draw(0, 100);
+				title.draw(40,100);
+
+				whiteBase10(L"ver.0.0.1").draw(600, 250 - whiteBase10.ascent, Color(0, 0, 0));
+				migMixB20(L"α").draw(720, 250 - migMixB20.ascent, Color(0, 0, 0));
+
+				gameStart.draw(Color(0, 0, 0, 0));
+				overDrive20(L"Game Start").draw(80, 320, Color(0, 0, 0, textAlpha_0));
+				credits.draw(Color(0, 0, 0, 0));
+				overDrive20(L"Credits").draw(80, 440, Color(0, 0, 0, textAlpha_2));
+				exit.draw(Color(0, 0, 0, 0));
+				overDrive20(L"Exit").draw(80, 500, Color(0, 0, 0, textAlpha_3));
+
+				const bool isMouse_0 = gameStart.mouseOver;
+				const bool isMouse_2 = credits.mouseOver;
+				const bool isMouse_3 = exit.mouseOver;
+
+				if(isMouse_0){
+					if (textAlpha_0 >= 255) {
+						isTextAlpha_0 = true;
+					}
+					else if (textAlpha_0 <= 100) {
+						isTextAlpha_0 = false;
+					}
+					if (isTextAlpha_0 == true) {
+						textAlpha_0 -= 5;
+					}
+					else if (isTextAlpha_0 == false) {
+						textAlpha_0 += 5;
+					}
+				}
+				else if(! isMouse_0){
+					if (textAlpha_0 < 255) {
+						textAlpha_0 += 5;
+					}
+					if (textAlpha_0 >= 255) {
+						isTextAlpha_0 = true;
+					}
+				}
+				if (isMouse_2) {
+					if (textAlpha_2 >= 255) {
+						isTextAlpha_2 = true;
+					}
+					else if (textAlpha_2 <= 100) {
+						isTextAlpha_2 = false;
+					}
+					if (isTextAlpha_2 == true) {
+						textAlpha_2 -= 5;
+					}
+					else if (isTextAlpha_2 == false) {
+						textAlpha_2 += 5;
+					}
+				}
+				else if (!isMouse_2) {
+					if (textAlpha_2 < 255) {
+						textAlpha_2 += 5;
+					}
+					if (textAlpha_2 >= 255) {
+						isTextAlpha_2 = true;
+					}
+				}
+				if (isMouse_3) {
+					if (textAlpha_3 >= 255) {
+						isTextAlpha_3 = true;
+					}
+					else if (textAlpha_3 <= 100) {
+						isTextAlpha_3 = false;
+					}
+					if (isTextAlpha_3 == true) {
+						textAlpha_3 -= 5;
+					}
+					else if (isTextAlpha_3 == false) {
+						textAlpha_3 += 5;
+					}
+				}
+				else if (!isMouse_3) {
+					if (textAlpha_3 < 255) {
+						textAlpha_3 += 5;
+					}
+					if (textAlpha_3 >= 255) {
+						isTextAlpha_3 = true;
+					}
+				}
+				
 
 			break;
 
 		}
-
 		case Scene::Stage:{
 
 			void updateGame(); {
-				void updateWindow(); {
-					//closeをクリックで閉じる
-					if (close.leftClicked)
-					{
-						System::Exit();
-					}
-					//ウインドウの移動
-					if (menubar.leftClicked)
-					{
-						// メニューをクリックしたらつかむ
-						if (menubarGrabbed == false)
-						{
-							//メニューつかんでないときマウス座標更新
-							mousePos = Mouse::Pos();
 
-						}
-						menubarGrabbed = true;
-
-					}
-					else if (Input::MouseL.released)
-					{
-						// マウスのクリックが離されたら離す
-						menubarGrabbed = false;
-					}
-					else if (menubarGrabbed)
-					{
-						// つかんでいる間はマウスのポジションに
-						Window::SetPos(Mouse::ScreenPos() - mousePos);
-					}
-
-				}
 
 				if (menubarGrabbed == false) {
 					void updateEnemy(); {
@@ -141,7 +237,6 @@ void Main()
 
 			}
 			void drawGame(); {
-				rect.draw(Color(255, 255, 255));
 				void drawEnemy(); {
 					enemy.draw();
 				}
@@ -155,25 +250,26 @@ void Main()
 						b.draw();
 					}
 				}
-				void drawWindow(); {
-					rect.drawFrame(30, 0, Color(0, 0, 0));
 
-					if (menubarGrabbed == true) {
-						rect.draw(Color(0, 0, 0, 200));
-					}
-					menubar.draw(Color(0, 0, 0));
-					//fontMin(L"DAINSLEIF").draw(8, 4);
-					close.draw(Color(0, 0, 0));
-					Line(778, 8, 792, 22).draw(2);
-					Line(792, 8, 778, 22).draw(2);
-
-					fontMin(player.get_position()).draw();
-				}
 				testCircle.draw(Color(255, 0, 0, 80));
 				break;
 
 			}
 			}
+		}
+		void drawWindowFrame(); {
+			rect.drawFrame(30, 0, Color(0, 0, 0));
+
+			if (menubarGrabbed == true) {
+				rect.draw(Color(0, 0, 0, 200));
+			}
+			menubar.draw(Color(0, 0, 0));
+			//fontMin(L"DAINSLEIF").draw(8, 4);
+			close.draw(Color(0, 0, 0));
+			Line(778, 8, 792, 22).draw(2);
+			Line(792, 8, 778, 22).draw(2);
+
+			fontMin(player.get_position()).draw();
 		}
 	}
 }
